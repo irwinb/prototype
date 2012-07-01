ad1215c0b405f0e99bdfaaa17267710603ffb5e5
andrewcarvalho <aj.carvalho@andrewcarvalho.com>
2012-07-01 18:51:05 -0400

    adding initial project files

---
+++ IrwinPrototype/src/com/onebitpixel/shooter/GameShooter.java
@@ -0,0 +1,47 @@
+package com.onebitpixel.shooter;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+import com.onebitpixel.shooter.Screens.*;
+
+public class GameShooter implements ApplicationListener {
+	private GameScreen gameScreen;
+	
+	@Override
+	public void create() {		
+		
+		gameScreen = new GameScreen();
+		
+	}
+
+	@Override
+	public void dispose() {
+	}
+
+	@Override
+	public void render() {		
+		gameScreen.render(Gdx.graphics.getDeltaTime());
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+	
+}
---
+++ IrwinPrototype/src/com/onebitpixel/shooter/Screens/GameScreen.java
@@ -0,0 +1,83 @@
+package com.onebitpixel.shooter.Screens;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.physics.box2d.joints.MouseJoint;
+
+public class GameScreen implements Screen {
+	
+	private float WORLD_WIDTH = 64f;
+	private float WORLD_HEIGHT = 36f;
+	
+	OrthographicCamera camera = new OrthographicCamera();
+	
+	public static final int VELOCITY_ITERATIONS = 5;
+	public static final int POSITION_ITERATIONS = 5;
+	private World world = new World(new Vector2(0.0f, -9.81f), true);
+	private Box2DDebugRenderer renderer = new Box2DDebugRenderer();
+	
+	private MouseJoint mouseJoint;
+	private Body mouseBody;
+	private Body hitBody;
+	
+	private SpriteBatch batch;
+	
+	public GameScreen() {
+		BodyDef bodyDef = new BodyDef();
+		mouseBody = world.createBody(bodyDef);
+
+		batch = new SpriteBatch();
+	}
+	
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void update(float delta) {
+		world.step(delta, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
+	}
+
+	@Override
+	public void render(float delta) {
+		update(delta);
+		
+		camera.update();
+		renderer.render(world, camera.combined);
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		camera.setToOrtho(true, WORLD_WIDTH, WORLD_HEIGHT);
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
